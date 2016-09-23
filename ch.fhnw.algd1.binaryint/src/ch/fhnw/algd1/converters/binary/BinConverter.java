2094f535e0c9423d6bff7c000cc752ecfdaae06a
Pascal Lüscher <luescherpascal@gmail.com>
2016-09-23 14:36:26 +0200

    tostring gemacht

--- ch.fhnw.algd1.binaryint/src/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ ch.fhnw.algd1.binaryint/src/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -1,15 +1,42 @@
 package ch.fhnw.algd1.converters.binary;
 
-
-
 public class BinConverter {
 	public static String toString(int x) {
-		// TODO: expect x to be in range [-128, 127], return string with 8 binary digits representing x in 2-complement
-		return "00000000";
+		// TODO: expect x to be in range [-128, 127], return string with 8
+		// binary digits representing x in 2-complement
+
+		String pattern = "00000000"; // 8 * 0
+		if (x == 0)
+			return pattern;
+
+		StringBuilder sb = new StringBuilder();
+		int y = x;
+
+		if (y < 0)
+			y = (int) (Math.pow(2, 8) + y);
+
+		while (y > 0) {
+			if ((y % 2) == 0)
+				sb.insert(0, "0");
+			else
+				sb.insert(0, "1");
+			y /= 2;
+		}
+		String fromSB = sb.toString();
+		String formatted = (pattern + fromSB).substring(fromSB.length());
+		if (x > 0)
+			return formatted;
+
+		sb = new StringBuilder(formatted);
+		sb.setCharAt(0, '1');
+		return sb.toString();
 	}
 
 	public static int fromString(String text) {
-		// TODO: expect text to contain 8 binary digits, parse to int value in 2-complement
-		return 0;
+		int retVal = 0;
+		for (int i = 0; i < text.length(); i++) {
+
+		}
+		return retVal;
 	}
 }
